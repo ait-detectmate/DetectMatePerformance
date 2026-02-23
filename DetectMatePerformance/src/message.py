178c5ef4c107f5ce4e52d3f2cde1c8c441ac7d10
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-02-23 13:17:02 +0100

    reshape and refactor log, templates python classes

--- DetectMatePerformance/src/message.py
+++ DetectMatePerformance/src/message.py
@@ -1,37 +1,57 @@
 from DetectMatePerformance.src.message_class import Messages, Templates
 
 
-class LogTemplates:
+def _load_file(path: str) -> list[str]:
+    with open(path, "r") as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+class _BasicMessage:
+    def __init__(self, messages: list[str], inst: object):
+        self.inst = inst(messages)
+
+    def __len__(self) -> int:
+        return self.inst.size()
+
+    def shape(self) -> tuple[int, int]:
+        return self.inst.shape()
+
+    def get_next(self) -> list[str]:
+        return self.inst.get_next_message()
+    
+    def __eq__(self, other: "_BasicMessage") -> bool:
+        if self.shape() != other.shape():
+            return False
+        
+        while (x := self.get_next()) != []:
+            if x != other.get_next():
+                return False
+            
+        return True
+
+
+class LogTemplates(_BasicMessage):
     def __init__(self, templates: list[str]):
-        self.templates = Templates(
-            map(lambda x: x.replace("<*>", "VAR"), templates)
+        super().__init__(
+            map(lambda x: x.replace("<*>", "VAR"), templates),
+            Templates
         )
 
     def __str__(self) -> str:
         return f"LogTemplates(shape={self.shape()})"
-
-    def __len__(self) -> int:
-        return self.templates.size()
-
-    def shape(self) -> tuple[int, int]:
-        return self.templates.shape()
     
-    def get_next(self) -> list[str]:
-        return self.templates.get_next_message()
+    @classmethod
+    def from_file(cls, path: str) -> "LogTemplates":
+        return cls(_load_file(path))
 
 
-class Logs:
+class Logs(_BasicMessage):
     def __init__(self, logs: list[str]):
-        self.messages = Messages(logs)
+        super().__init__(logs, Messages)
 
     def __str__(self) -> str:
         return f"Logs(shape={self.shape()})"
 
-    def __len__(self) -> int:
-        return self.messages.size()
-
-    def shape(self) -> tuple[int, int]:
-        return self.messages.shape()
-    
-    def get_next(self) -> list[str]:
-        return self.messages.get_next_message()
+    @classmethod
+    def from_file(cls, path: str) -> "Logs":
+        return cls(_load_file(path))
--- DetectMatePerformance/tests/test_python/test_messages.py
+++ DetectMatePerformance/tests/test_python/test_messages.py
@@ -1,6 +1,9 @@
 from DetectMatePerformance.src.message import Logs, LogTemplates
 
 
+path_temp = "DetectMatePerformance/tests/test_data/audit_templates.txt"
+path_logs = "DetectMatePerformance/tests/test_data/audit.log"
+
 msg = [
     "Hello, world!", "Goodbye, world! goodbye"
 ]
@@ -21,6 +24,14 @@ class TestCaseMessage:
         assert logs.get_next() == ["Hello", "world"]
         assert logs.get_next() == ["Goodbye", "world", "goodbye"]
         assert logs.get_next() == []
+
+    def test_from_file(self):
+        log_temp = Logs.from_file(path_logs)
+        assert log_temp.shape() == (2316, 60)
+
+    def test_eq(self):
+        assert Logs(msg) == Logs(msg)
+        assert Logs(msg) != Logs(["hi"])
 
 
 templates = [
@@ -43,3 +54,11 @@ class TestCaseTemplates:
         assert log_temp.get_next() == ["Hello", "VAR", "world", "VAR"]
         assert log_temp.get_next() == ["Goodbye", "VAR", "world", "VAR"]
         assert log_temp.get_next() == []
+
+    def test_from_file(self):
+        log_temp = LogTemplates.from_file(path_temp)
+        assert log_temp.shape() == (9, 50)
+
+    def test_eq(self):
+        assert LogTemplates(msg) == LogTemplates(msg)
+        assert LogTemplates(msg) != LogTemplates(["hi"])
