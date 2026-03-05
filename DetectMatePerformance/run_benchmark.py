4fc077e5b9fea78b7af4bb5d836902811ec80aeb
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-03-05 10:29:44 +0100

    format benchmark

--- DetectMatePerformance/run_benchmark.py
+++ DetectMatePerformance/run_benchmark.py
@@ -2,21 +2,24 @@
 
 from DetectMatePerformance.src.match_tree import TreeMatcher
 
-path_temp = "DetectMatePerformance/tests/test_data/audit_templates.txt"
-path_logs = "DetectMatePerformance/tests/test_data/audit.log"
-regex = r"type=(?P<Type>\w+) msg=audit\((?P<Time>[^:]+):(?P<Serial>\d+)\): (?P<Content>.*)"
+datasets = [
+    {
+        "name": "AIT - Audit",
+        "path_temp": "DetectMatePerformance/tests/test_data/audit_templates.txt",
+        "path_logs": "DetectMatePerformance/tests/test_data/audit.log",
+        "regex": r"type=(?P<Type>\w+) msg=audit\((?P<Time>[^:]+):(?P<Serial>\d+)\): (?P<Content>.*)"
+    }
+]
 
-def load_logs() -> list[str]:
-    logs = []
+def load_logs(path_logs) -> list[str]:
     with open(path_logs, "r") as f:
-        for log in f.readlines():
-            logs.append(log)
-    return logs
-
-print(tree_matcher := TreeMatcher.from_file(path_temp))
-
-logs = load_logs()
-print(f"Num logs: {len(logs)}")
+        return f.readlines()
 
 
-print(tree_matcher(logs, get_var=True, regex=regex))
+
+for dataset in datasets:
+    logs = load_logs(dataset["path_logs"])
+    print(tree_matcher := TreeMatcher.from_file(dataset["path_temp"]))
+    print(f"Num logs: {len(logs)} from {dataset['name']}")
+    print(tree_matcher(logs, get_var=True, regex=dataset["regex"]))
+    print("---------------------------------------------------\n")
