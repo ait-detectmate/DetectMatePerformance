a06760e3917e3a4d6c4934fad53f7d865c179914
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-03-23 16:15:03 +0100

    add new eventids in pipeline

--- src/detectmateperformance/pipeline_op.py
+++ src/detectmateperformance/pipeline_op.py
@@ -33,21 +33,16 @@ def add_parsed(df: pl.DataFrame, results: ParsedLogs) -> pl.DataFrame:
 
     if "Templates" in df:
         df = df.with_columns(pl.Series("Templates", results.get_all_templates()))
+        df = df.with_columns(pl.Series("EventIDs", results.get_all_events_ids()))
     else:
         df = df.insert_column(df.shape[1], pl.Series("Templates", results.get_all_templates()))
+        df = df.insert_column(df.shape[1], pl.Series("EventIDs", results.get_all_events_ids()))
 
     return df
 
 
 def postprocessing(df: pl.DataFrame) -> pl.DataFrame:
     df = df.with_columns(pl.col("Templates").str.replace_all("VAR", "<*>"))
-    eventids = {e: i for i, e in enumerate(df["Templates"].unique())}
-    df.insert_column(
-        df.shape[1] - 1, df.map_rows(
-            lambda x: eventids[x[df.columns.index("Templates")]]
-        ).to_series().rename("EventIDs")
-    )
-
     return df
 
 
