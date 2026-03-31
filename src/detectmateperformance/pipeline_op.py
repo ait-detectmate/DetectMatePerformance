6adbb97ecb428623849a33f9b24b21dab36485f8
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-03-31 10:50:36 +0200

    add hotfix content missing

--- src/detectmateperformance/pipeline_op.py
+++ src/detectmateperformance/pipeline_op.py
@@ -88,6 +88,10 @@ def run_full_pipeline(
     print(">>> Preprocesing logs")
     table = preprocessing(logs, regex=regex)
 
+    if "Content" not in table.columns:
+        print("!Content not found")
+        return table
+
     return run_batches(
         func=func,
         table=table,
