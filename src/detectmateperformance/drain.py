63dd35945a4e8512d17d005241aaec50d13f0965
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-07-22 09:21:01 +0200

    remove previous stream drain version

--- src/detectmateperformance/drain.py
+++ src/detectmateperformance/drain.py
@@ -1,5 +1,4 @@
 import polars as pl
-import re
 
 
 def cluster_logs_df(df: pl.DataFrame, depth: int = 2, max_child: int = 10) -> dict[str, list[str]]:
@@ -25,18 +24,6 @@ def cluster_logs_df(df: pl.DataFrame, depth: int = 2, max_child: int = 10) -> di
 
     dict_ = df.group_by("concat_list").agg(pl.col("Content")).rows_by_key("concat_list")
     return {k: v[0][0] for k, v in dict_.items()}
-
-
-def cluster_logs_stream(log: str, depth: int = 2, max_child: int = 10) -> dict[str, list[str]]:
-    log = re.sub(r"[^a-zA-Z0-9\s]", " ", log)
-    log = re.sub(r"\d+", "<*>", log)
-    log = re.sub(r"\s+", " ", log)
-    log_split = log.split(" ")
-
-    key = f"{n if (n := len(log_split)) < max_child else max_child}"
-    for i in range(depth):
-        key += f"_{'None' if i >= n else log_split[i]}"
-    return {key: [log]}
 
 
 class Drain:
--- tests/test_python/test_drain.py
+++ tests/test_python/test_drain.py
@@ -25,26 +25,3 @@ class TestCommonMethods:
             '4_hello_world_<*>_ciao_None_None_None_None_None_None': ['hello world <*> ciao']
         }
         assert drain.cluster_logs_df(df, depth=10) == expected
-
-    def test_cluster_logs_stream(self):
-        logs = ["hello world, 12 ciao", "Ciao! ? bella bella"]
-
-        assert drain.cluster_logs_stream(logs[0], depth=2) == {'4_hello_world': ['hello world <*> ciao']}
-        assert drain.cluster_logs_stream(logs[1], depth=2) == {'3_Ciao_bella': ['Ciao bella bella']}
-
-        assert drain.cluster_logs_stream(logs[0], depth=3) == {'4_hello_world_<*>': ['hello world <*> ciao']}
-        assert drain.cluster_logs_stream(logs[1], depth=3) == {'3_Ciao_bella_bella': ['Ciao bella bella']}
-
-    def test_cluster_logs_stream_special_cases(self):
-        logs = ["hello world, 12 ciao", "Ciao! ? bella bella"]
-
-        assert drain.cluster_logs_stream(logs[0], depth=1, max_child=2) == {
-            '2_hello': ['hello world <*> ciao']
-        }
-        assert drain.cluster_logs_stream(logs[1], depth=1, max_child=2) == {'2_Ciao': ['Ciao bella bella']}
-
-        expected = {'4_hello_world_<*>_ciao_None_None_None_None_None_None': ['hello world <*> ciao']}
-        assert drain.cluster_logs_stream(logs[0], depth=10) == expected
-
-        expected = {'3_Ciao_bella_bella_None_None_None_None_None_None_None': ['Ciao bella bella']}
-        assert drain.cluster_logs_stream(logs[1], depth=10) == expected
