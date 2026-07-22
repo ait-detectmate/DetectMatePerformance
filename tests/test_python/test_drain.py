909b8570b5fb92c218675f0dd952319e9ef796d2
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-07-22 13:38:36 +0200

    update unittests

--- tests/test_python/test_drain.py
+++ tests/test_python/test_drain.py
@@ -8,21 +8,21 @@ class TestCommonMethods:
     def test_cluster_logs_df(self):
         df = pl.DataFrame({"Content": ["hello world, 12 ciao", "Ciao! ? bella bella"]})
 
-        expected = {'3_Ciao_bella': ['Ciao bella bella'], '4_hello_world': ['hello world <*> ciao']}
+        expected = {'3_Ciao_bella': ['Ciao bella bella'], '4_hello_world': ['hello world VAR ciao']}
         assert drain.cluster_logs_df(df, depth=2) == expected
 
-        expected = {'3_Ciao_bella_bella': ['Ciao bella bella'], '4_hello_world_<*>': ['hello world <*> ciao']}
+        expected = {'3_Ciao_bella_bella': ['Ciao bella bella'], '4_hello_world_VAR': ['hello world VAR ciao']}
         assert drain.cluster_logs_df(df, depth=3) == expected
 
     def test_cluster_logs_df_special_cases(self):
         df = pl.DataFrame({"Content": ["hello world, 12 ciao", "Ciao bella bella"]})
 
-        expected = {'2_Ciao': ['Ciao bella bella'], '2_hello': ['hello world <*> ciao']}
+        expected = {'2_Ciao': ['Ciao bella bella'], '2_hello': ['hello world VAR ciao']}
         assert drain.cluster_logs_df(df, depth=1, max_child=2) == expected
 
         expected = {
             '3_Ciao_bella_bella_None_None_None_None_None_None_None': ['Ciao bella bella'],
-            '4_hello_world_<*>_ciao_None_None_None_None_None_None': ['hello world <*> ciao']
+            '4_hello_world_VAR_ciao_None_None_None_None_None_None': ['hello world VAR ciao']
         }
         assert drain.cluster_logs_df(df, depth=10) == expected
 
