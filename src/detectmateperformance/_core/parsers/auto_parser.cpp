e0e8ad8f26b3226afe98687dd219e7c772422af4
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-08-28 11:56:02 +0200

    minor change

---
+++ src/detectmateperformance/_core/parsers/auto_parser.cpp
@@ -0,0 +1,11 @@
+
+#include "auto_parser.h"
+
+
+Templates auto_parser_generator(std::vector<std::string> sentences) {
+
+
+    Templates templates("hi");
+
+    return templates;
+}
---
+++ src/detectmateperformance/_core/parsers/auto_parser.h
@@ -0,0 +1,13 @@
+#ifndef M_AUTOPARSER_H
+#define M_AUTOPARSER_H
+
+#include <vector>
+#include <string>
+
+#include "../_type/templates.h"
+#include "../template_matcher/match_tree.h"
+
+
+Templates auto_parser_generator(std::vector<std::string> sentences);
+
+#endif
