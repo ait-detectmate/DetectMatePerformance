6f195bd56f7ba9e3fa6f45c49de4f1111e078b2d
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2026-09-01 12:42:28 +0200

    rename drain methods

--- src/detectmateperformance/_core/parsers/drain.h
+++ src/detectmateperformance/_core/parsers/drain.h
@@ -9,18 +9,18 @@
 #include "../_type/templates.h"
 
 
-float calculate_sim(std::string sentence_1, std::string sentence_2);
+float calculateSim(std::string sentence_1, std::string sentence_2);
 
-std::string generate_template(std::deque<std::string> sentences);
+std::string generateTemplate(std::deque<std::string> sentences);
 
-std::deque<std::string> generate_templates(
+std::deque<std::string> generateTemplates(
     std::vector<std::deque<std::string>> sentences, float simSeq
 );
 
-std::deque<std::string> clean_templates(std::deque<std::string> templates);
+std::deque<std::string> cleanTemplates(std::deque<std::string> templates);
 
 
-Templates drain_generator(
+Templates drainGenerator(
     std::vector<std::deque<std::string>> sentences, float SimSeq
 );
 
